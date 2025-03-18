59b8c0b499316eb48c2a451b885a58e08ed8e99d
Rowan McKereghan <rowanmckereghan@gmail.com>
2025-03-18 13:59:05 -0700

    some changes for frontend integration

--- src/altpoet/settings/sample_local_settings.py
+++ src/altpoet/settings/sample_local_settings.py
@@ -21,6 +21,28 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+CORS_ALLOW_HEADERS = ("content-type", "x-csrftoken")
+
+CORS_ALLOW_METHODS = (
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+)
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
